4bd9dd309fe92e22a30643741332e13e31c97bc4
NaytK <aleksswem@yandex.ru>
2022-05-20 21:53:21 +0300

    Stage №1

--- main.py
+++ main.py
@@ -5,5 +5,3 @@ import time
 cyrillic = [chr(letter) for letter in range(ord('А'), ord('я') + 1)]
 cyrillic_lower = [chr(letter) for letter in range(ord('а'), ord('я') + 1)]
 cyrillic_upper = [chr(letter) for letter in range(ord('А'), ord('Я') + 1)]
-
-cyrillic_upper = [chr(letter) for letter in range(ord('А'), ord('Я') + 1)]
