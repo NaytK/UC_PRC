08171a460b4ab1902f21216a682d2943cf7372d2
NaytK <aleksswem@yandex.ru>
2022-05-20 17:52:52 +0300

    Stage №1

---
+++ main.py
@@ -0,0 +1,7 @@
+import string
+import time
+
+
+cyrillic = [chr(letter) for letter in range(ord('А'), ord('я') + 1)]
+cyrillic_lower = [chr(letter) for letter in range(ord('а'), ord('я') + 1)]
+cyrillic_upper = [chr(letter) for letter in range(ord('А'), ord('Я') + 1)]
